0f75a034fbb7e814fc475a57e8fc2a3385fa6e2e
Luis Piñón <103865143+Lehp26@users.noreply.github.com>
2022-07-30 22:20:40 -0500

    Add files via upload

---
+++ Empresa_Hielo.sql
@@ -0,0 +1,55 @@
+-- CREATE DATABASE EMPRESA_HIELO;
+-- SHOW DATABASES;
+
+-- DROP TABLE PRODUCTOS;
+-- DROP TABLE CLIENTES;
+-- DROP TABLE EMPLEADOS;
+
+
+-- Create the table 'productos' that will be used to save all the products
+-- and chracteristics like 'Name', 'Quantity' and 'Price'
+-- CREATE TABLE PRODUCTOS(
+          -- ID INTEGER NOT NULL, -- PRIMARY KEY
+          -- PRODUCTO VARCHAR(20),     -- The abbreviate name of the product
+          -- CANTIDAD INTEGER,    -- The available quantity of products to be purchased
+          -- PRECIO DECIMAL(7,2),  -- The price of each products with and 2 digits after the decimal point
+          -- PRIMARY KEY (ID) 
+          -- );
+
+-- CREATE TABLE CLIENTES(
+		  -- ID INTEGER NOT NULL, -- PRIMARY KEY
+          -- PRODUCTO varchar(20), -- The name of the business
+          -- PRECIO DECIMAL(7,2),
+		  -- PRIMARY KEY(ID)
+          -- );
+          
+-- CREATE TABLE EMPLEADOS(
+          -- ID INTEGER NOT NULL, -- PRIMARY KEY
+          -- NOMBRE varchar(30),  -- The name of the employee
+          -- VENTAS_TOTALES DECIMAL(8,2),
+          -- PRIMARY KEY(ID)
+          -- );
+          
+-- INSERT INTO CLIENTES VALUES 
+-- (1,'A',120.00),
+-- (2,'B',90.00),
+-- (3,'C',80.00),
+-- (4,'D',75.00);
+
+-- INSERT INTO EMPLEADOS VALUES 
+-- (1,'Oscar',100.00),
+-- (2,'Luis',150.00),
+-- (3,'Gabriel',140.00),
+-- (4,'Juan',90.00);
+
+-- INSERT INTO PRODUCTOS VALUES
+-- (1,'A',13,150.00),
+-- (2,'B',50,200.00),
+-- (3,'C',7,78.00),
+-- (4,'D',130,123.00),
+-- (5,'D',145,250.00),
+-- (6,'E',302,124.00),
+-- (7,'G',407,374.00),
+-- (8,'H',301,102.00),
+-- (9,'I',402,174.00),
+-- (10,'I',125,230.00);
